c2a9700f4d3119b8da3fe8c2f4049ecaa141b073
Andrew Stec <andrew@Node-6.9.1-Server>
2017-03-25 22:32:01 +0000

    First commit. Webpack compiles

---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+let server = require('http').Server(app);
+
+//Uses the environment port or defaults to 3000
+let port = process.env.PORT || 3000
+
+//Serves the static files from /public directory
+
+app.use(express.static('public'));
+
+//Creates an endpoint which returns the index.html file
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+//Starts the server
+server.listen(port, () => console.log('server has started on port ' + port))
