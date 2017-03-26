3069246905e34f2ea32e7b5bfae7ba0d5b27d596
Andrew Stec <andrew@Node-6.9.1-Server>
2017-03-26 00:45:25 +0000

    created Dockerapi plugin for server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let express = require('express');
 let path = require('path');
 let app = express();
 let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let docker = require('dockerapi');
 
 //Uses the environment port or defaults to 3000
 let port = process.env.PORT || 3000
